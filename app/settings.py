382332df6ee2ecf92db35d0b977ac68aac89fbed
diegouidev <diegocoelho44@gmail.com>
2025-10-27 10:36:56 -0300

    atualizando settings

--- app/settings.py
+++ app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c5##wt(b&3!po^z*ya0f-y=c#!)2tm$wcyamu3e+*f7f9+9(p!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['diego254680.pythonanywhere.com']
 
 
 # Application definition
